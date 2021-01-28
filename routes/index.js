c2b623dcb990d2b029eff5adc6154aad897373f7
root-marco <66764981+root-marco@users.noreply.github.com>
2021-01-28 12:18:56 -0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
-const express = require('express')
+import express from 'express';
+
 const router = express.Router()
 
 router.get('/', (req, res) => {
     res.render('index')
 })
 
-module.exports = router
+export default router;
