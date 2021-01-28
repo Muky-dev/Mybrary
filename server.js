7b8ce7afd0fb267af0e1dc0281cbab9a401afe20
root-marco <66764981+root-marco@users.noreply.github.com>
2021-01-28 12:17:53 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,31 +1,33 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
-}
+import mongoose from 'mongoose';
+import express from 'express';
+import expressLayouts from 'express-ejs-layouts';
+import path from 'path';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const express = require('express');
+import indexRouter from './routes/index.js';
+
 const app = express();
-const expressLayouts = require('express-ejs-layouts');
-
-const indexRouter = require('./routes/index');
 
 app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
+app.set('views', path.resolve() + '/views');
 app.set('layout', 'layouts/layout');
+
 app.use(expressLayouts);
 app.use(express.static('public'));
 
-const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+	useNewUrlParser: true,
+	useUnifiedTopology: true
 });
 const db = mongoose.connection;
 db.on('error', error => console.error(error));
 db.once('open', () => {
-    console.log('Connected to Mongoose')
+	console.log('Connected to Mongoose')
 });
 
 app.use('/', indexRouter)
 
-
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, () => {
+	console.log('server running');
+});
