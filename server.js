8b52974012916acd8f0a95cc418a00085cefbd94
Muky-dev <69209901+Muky-dev@users.noreply.github.com>
2021-04-07 12:31:30 -0300

    Books Create/Index/New

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
+	require('dotenv').config();
 }
 
 const express = require('express');
@@ -9,27 +9,29 @@ const bodyParser = require('body-parser');
 
 const indexRouter = require('./routes/index');
 const authorRouter = require('./routes/authors');
+const bookRouter = require('./routes/books');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
+app.use(express.urlencoded({ limit: '10mb', extended: false }))
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+	useNewUrlParser: true,
+	useUnifiedTopology: true
 });
 const db = mongoose.connection;
 db.on('error', error => console.error(error));
 db.once('open', () => {
-    console.log('Connected to Mongoose')
+	console.log('Connected to Mongoose')
 });
 
 app.use('/', indexRouter)
 app.use('/authors', authorRouter)
+app.use('/books', bookRouter)
 
 
 app.listen(process.env.PORT || 3000);
