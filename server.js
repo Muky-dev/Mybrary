f2c4bdb3dd37e401e3ec5ff00ea7c2a48e220296
Muky-dev <69209901+Muky-dev@users.noreply.github.com>
2021-08-10 20:10:40 -0300

    Added method override

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
-const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 
 const indexRouter = require('./routes/index');
 const authorRouter = require('./routes/authors');
@@ -15,8 +15,9 @@ app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
+app.use(methodOverride('_method'));
 app.use(express.static('public'));
-app.use(express.urlencoded({ limit: '10mb', extended: false }))
+app.use(express.urlencoded({ limit: '10mb', extended: false }));
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL, {
@@ -25,9 +26,7 @@ mongoose.connect(process.env.DATABASE_URL, {
 });
 const db = mongoose.connection;
 db.on('error', error => console.error(error));
-db.once('open', () => {
-	console.log('Connected to Mongoose')
-});
+db.once('open', () => console.log('Connected to Mongoose'));
 
 app.use('/', indexRouter)
 app.use('/authors', authorRouter)
