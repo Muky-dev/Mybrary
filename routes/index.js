db23c2d821c9b31fa97e430163854f70bcc1373d
Muky-dev <69209901+Muky-dev@users.noreply.github.com>
2021-08-10 20:35:10 -0300

    Minor fixes

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,13 @@ const router = express.Router()
 const Book = require('../models/book')
 
 router.get('/', async (req, res) => {
-    let book
+    let books
     try {
-        books = await Books.find().sort({ createdAt: 'desc' }).limit(10).exec()
+        books = await Book.find().sort({ createdAt: 'desc' }).limit(10).exec()
     } catch {
         books = []
     }
-    res.render('index', {books: books})
+    res.render('index', { books: books })
 })
 
 module.exports = router
