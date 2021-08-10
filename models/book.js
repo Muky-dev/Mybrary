338e09b699a5ec233646fa3b25957250d0d688f4
Muky-dev <69209901+Muky-dev@users.noreply.github.com>
2021-08-10 20:23:18 -0300

    Changed how book model works

--- models/book.js
+++ models/book.js
@@ -1,6 +1,4 @@
 const mongoose = require('mongoose')
-const path = require('path')
-const coverImageBasePath = 'uploads/bookCovers';
 
 const bookSchema = new mongoose.Schema({
 	title: {
@@ -23,7 +21,11 @@ const bookSchema = new mongoose.Schema({
 		required: true,
 		default: Date.now
 	},
-	coverImageName: {
+	coverImage: {
+		type: Buffer,
+		required: true
+	},
+  	coverImageType: {
 		type: String,
 		required: true
 	},
@@ -35,10 +37,9 @@ const bookSchema = new mongoose.Schema({
 })
 
 bookSchema.virtual('coverImagePath').get(function() {
-	if (this.coverImageName != null) {
-		return path.join('/', coverImageBasePath, this.coverImageName)
-	}
+  if (this.coverImage != null && this.coverImageType != null) {
+	return `data:${this.coverImageType};charset=utf-8;base64,${this.coverImage.toString('base64')}`
+  }
 })
 
 module.exports = mongoose.model('Book', bookSchema)
-module.exports.coverImageBasePath = coverImageBasePath
